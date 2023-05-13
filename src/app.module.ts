ff454da27ea2b451b98a64f4a7968b144d75f18e
Fernando01santana <cool.fernando23@gmail.com>
2023-05-13 16:12:55 -0300

    feat: implements rate limiter

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AuthModule } from './modules/auth/auth.module';
 // import { TypeOrmModule } from './config/datasource/typeOrmModule';
 import { RedisModule } from '@nestjs-modules/ioredis';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { TypeOrmModule } from './config/typeOrmMudule';
 import { AddresssModule } from './modules/address/address.module';
 import { UsersModule } from './modules/users/users.module';
@@ -19,6 +20,10 @@ import { UsersModule } from './modules/users/users.module';
           url: 'redis://localhost:6379',
         },
       }),
+    }),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
     }),
   ],
 })
